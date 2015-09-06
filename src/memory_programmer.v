92064cac9777fa1bf65f1125d3cb231047c5e5a7
yoshiaki rikitake <yoshiaki@sendai-nct.ac.jp>
2015-09-06 20:33:50 +0900

    mod memory_programmer: add memory read mode

--- src/memory_programmer.v
+++ src/memory_programmer.v
@@ -1,26 +1,28 @@
 `default_nettype none
 
 module memory_programmer(
-  input wire  clock_in,
-  input wire  reset_N,
+  input wire        reset_N,
+  input wire        prog_clock,
+  input wire        prog_wr_en,
+  input wire  [7:0] prog_code,
+  output wire [7:0] data,
 
-  input wire [7:0]  data_in,
-
-  output wire       clock_out,
-  output wire       wr_en_out,
-  output wire [7:0] address_out,
-  output wire [7:0] data_out
+  output wire       pr_clock,
+  output wire       pr_wr_en,
+  output wire [7:0] pr_adrs,
+  output wire [7:0] pr_code,
+  input wire  [7:0] mm_data
   );
 
   reg [7:0] address_counter;
 
-  assign clock_out = ~clock_in;
-  assign wr_en_out = reset_N;
-  assign address_out = address_counter;
-  assign data_out = data_in;
+  assign pr_clock = ~prog_clock;
+  assign pr_wr_en = prog_wr_en;
+  assign pr_adrs  = address_counter;
+  assign pr_code  = prog_code;
+  assign data     = mm_data;
 
-
-  always @(posedge clock_in or negedge reset_N) begin
+  always @(posedge prog_clock or negedge reset_N) begin
     if (reset_N == 0) begin
       address_counter <= 8'h00;      
     end
